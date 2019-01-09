c1c17e59dade7e8f682763f9d4a42b0462c5d7d6
Devesh Kumar <deveshkumarsep12@gmail.com>
2019-01-09 13:44:36 +0530

    1st Commit

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,14 +1,18 @@
 // for ERC20 Standard defination
-
 pragma solidity ^0.5.0;
 
 contract DappToken{
     uint256 public totalSupply;
 
+    mapping(address => uint256) public balanceof;
+
     // Constructor
     //function DappToken() public {     // older way of defining cunstructor
-    constructor () public {
-        totalSupply = 1000000;      //state variable    
+    constructor (uint256 _initialSupply) public {
+        balanceof[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;      //state variable
+        // Allocation of Initial Supply
+
     }
 
     // Set the tockens up
