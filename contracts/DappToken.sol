d9baf146325996dc6c0bd19a01a08b97fa8e4e4f
Devesh Kumar <deveshkumarsep12@gmail.com>
2019-01-13 22:37:46 +0530

    2. commit

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -11,13 +11,23 @@ contract DappToken{
     string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
+    // transfer
     event Transfer(
         address indexed _from,
         address indexed _to,
         uint256 _value
     );
 
+    // approve
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    // allowance
+    mapping(address => mapping(address => uint256)) public allowance; //nested variable declaration
 
     // Constructor
     //function DappToken() public {     // older way of defining cunstructor
@@ -29,7 +39,6 @@ contract DappToken{
     }
 
     // Transfer 
-    
     function transfer(address _to, uint256 _value) public returns (bool success) {
         // Exception if account has no sufficient balance
         require(balanceOf[msg.sender] >= _value, "Server Un-Authorized");
@@ -39,6 +48,29 @@ contract DappToken{
         // Trigger a transfer event
         emit Transfer(msg.sender, _to, _value);
         // Boolean Return  
+        return true;
+    }
+
+    // Delegates Transfer
+    // Aprove
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        //allowance
+        allowance[msg.sender][_spender] = _value;
+        //approve events
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    // Transfer from
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        //requirements _from have tokens
+        require (_value <= balanceOf[_from]);
+        // require allowance is big to transfer
+        require(_value <= allowance[_from][msg.sender]);
+        // change the balance
+        // update the allowance 
+        // call transfer event
+        // retrun boolian
         return true;
     }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -12,9 +12,10 @@ contract('DappToken', function (accounts) {
             return tokenInstance.symbol();
         }).then(function (symbol) {
             assert.equal(symbol, 'DAPP', 'has the correct symbol');
-        });//.then(function(standard) {
-          //  assert.equal(standard, "DApp Token v1.0", 'has the correct standard');
-        //});
+            return tokenInstance.standard();
+        }).then(function(standard) {
+            assert.equal(standard, "DApp Token v1.0", 'has the correct standard');
+        });
     });
 
     it("allocates the Initial supply upon deployment", function() {
@@ -56,5 +57,59 @@ contract('DappToken', function (accounts) {
         }).then(function (balance) {
             assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
         })
+    });
+
+
+    it('approves tokens for delegated transfer', function () {
+        return DappToken.deployed().then(function (instance) {
+            tokenInstance = instance;
+            return tokenInstance.approve.call(accounts[1], 100);
+        }).then(function (success) {
+            assert.equal(success, true, 'it returns true')
+            return tokenInstance.approve(accounts[1], 100);
+        }).then(function (receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+            assert.equal(receipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are authorized by');
+            assert.equal(receipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are authorized to');
+            assert.equal(receipt.logs[0].args._value, 100, 'logs the transfer amount');
+            return tokenInstance.allowance(accounts[0], accounts[1]);
+        }).then(function (allowance) {
+            assert.equal(allowance, 100, 'stores the allowance for delegated transfer');
+        })
+    });
+
+    it('handels delicated transfers', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            fromAccount = accounts[2];
+            toAccount = accounts[3];
+            spendingAccount = accounts[4];
+            // Transfer some tokens from from Acount
+            return tokenInstance.transfer(fromAccount, 100, {from:accounts[0]});
+        }).then(function(receipt) {
+            // Approve spending account to spend 10 token from fromAccount
+            return tokenInstance.approve(spendingAccount, 10, {from: fromAccount});
+        }).then(function(receipt) {
+            // try to transfer amount more then the balance
+            return tokenInstance.transferFrom(fromAccount, toAccount, 999999, {from: spendingAccount});
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger then balance');
+            // Try transfering some amount larger then the allowed account
+            return tokenInstance.transferFrom(fromAccount, toAccount, 20,{from:spendingAccount});
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'cannot transfer value larger then approved amount');
+            return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, {from: spendingAccount});
+        }).then(function(success) {
+            assert.equal(success, true);
+            return tokenInstance.transferForm(fromAccount, toAccount, 10, {from: spendingAccount});
+        }).then(function (receipt) {
+            assert.equal(receipt.logs.length, 1, 'triggers one event');
+            assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+            assert.equal(receipt.logs[0].args._from, fromAccount, 'logs of account tokens are transfered from');
+            assert.equal(receipt.logs[0].args._to, toAccount, 'logs of the account tokens are transfered to');
+            assert.equal(receipt.logs[0].args._value, 10, 'logs the transfer amount');
+            //2:38:25
+        });
     });
 });
